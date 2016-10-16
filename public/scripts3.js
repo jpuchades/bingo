c02dc86decf78ddeb6135bcfe44e6bbf8ef37dff
Jose Puchades <jpuchades@gmail.com>
2016-10-16 20:34:59 +0200

    Sin muchos cambios

--- public/scripts3.js
+++ public/scripts3.js
@@ -35,11 +35,14 @@ var genera_numero = function(){
 	return bola;
 }
 
-var dibuja_carton = function(coordenadas_carton){
-	for(var i = 0; i < coordenadas_carton.length; i++){
+var dibuja_carton = function(array){
+	for(var i = 0; i < array.length; i++){
 		var bola = genera_numero(VECTOR_NUMEROS);
-		coordenadas_carton[i]._b = bola;
+		array[i]._b = bola;
 	}
+	//console.log(array);
+
+	return array;
 }
 
 var coordenadas_carton = function(n, callback){
@@ -86,17 +89,28 @@ var extrae_bola = function(){
 	return bola;
 }
 
-var renderer = PIXI.autoDetectRenderer(760, 480);
-document.body.appendChild(renderer.view);
-var stage = new PIXI.Container();
+// declaramos los alias...
+var loader = PIXI.loader,
+	Container = PIXI.Container,
+	resources = PIXI.loader.resources,
+	autoDetectRenderer = PIXI.autoDetectRenderer,
+	TextureCache = PIXI.utils.TextureCache,
+	Sprite = PIXI.Sprite,
+	Rectangle = PIXI.Rectangle;
 
-PIXI.loader
+var stage = new Container(),
+	renderer = autoDetectRenderer(760, 480);
+document.body.appendChild(renderer.view);
+
+var array_bolas = [];
+
+loader
 	.add("background", "assets/bg.png")
 	.add("assets/balls-tileset.png")
 	.load(setup);
 
 function setup(){
-	var sprite_back = new PIXI.Sprite(PIXI.loader.resources.background.texture);
+	var sprite_back = new Sprite(resources.background.texture);
 	stage.addChild(sprite_back);
 
 	var style = {
@@ -105,20 +119,25 @@ function setup(){
 	    fontSize : '20px',
 	};
 
+	var numeros_cartones = [];
+	// mostramos los numeros de los cartones
 	for(var n = 0; n < CARTONES.length; n++){
 		las_coordenadas = coordenadas_carton(n, dibuja_carton);
-	
+			
 		for(var i = 0; i < las_coordenadas.length; i++){
 	
 			var numero = new PIXI.Text(las_coordenadas[i]._b, style);
 			numero.x = las_coordenadas[i]._x + (PADDING_CASILLA*2);
 			numero.y = las_coordenadas[i]._y + PADDING_CASILLA;
+
+			//numeros_cartones[las_coordenadas[i]._b] = {numero.x, numero.y};
+			numeros_cartones.push(las_coordenadas[i]);
+			
 			stage.addChild(numero);
 		}
 	}
-
-	var texture = PIXI.utils.TextureCache["assets/balls-tileset.png"];
-	
+	// tileset con todas las bolas	
+	// coordenadas para dibujar las bolas sacadas
 	var FILAS_BOLA = [
 		{x: 17, y: 338, total_bolas: 8},
 		{x: 17, y: 368, total_bolas: 7},
@@ -128,19 +147,28 @@ function setup(){
 
 	for(var f = 0; f < FILAS_BOLA.length; f++){
 		var shown = 0;
-		console.log("dibujar en: ("+ FILAS_BOLA[f].x + ", " + FILAS_BOLA[f].y +")");
-		while(shown <= FILAS_BOLA[f].total_bolas){
+		while(shown < FILAS_BOLA[f].total_bolas){
 			var b = extrae_bola();
-			console.log(b);
-			var rectangle = new PIXI.Rectangle(b.x, b.y, 30, 30);
+			console.log(b.valor);
+			var texture = TextureCache["assets/balls-tileset.png"];
+			var rectangle = new Rectangle(b.x, b.y, 30, 30);
 			texture.frame = rectangle;
-			var bola = new PIXI.Sprite(texture);
-			bola.x = FILAS_BOLA[f].x + (DIM_BOLA.ancho*shown);
-			bola.y = FILAS_BOLA[f].y;
-			console.log("[" + bola.x + ", " + bola.y + "]");
-			stage.addChild(bola);
-			shown++;			
+			array_bolas[b.valor] = new Sprite(texture);
+			array_bolas[b.valor].x = FILAS_BOLA[f].x + (DIM_BOLA.ancho * shown);
+			array_bolas[b.valor].y = FILAS_BOLA[f].y;
+			//console.log("[" + array_bolas[b.valor].x + ", " + array_bolas[b.valor].y + "]");
+			stage.addChild(array_bolas[b.valor]);
+			shown++;
+			
 		}
+
 	}
+
+//	console.log(array_bolas);
+
+
+
 	renderer.render(stage);
 }
+
+
